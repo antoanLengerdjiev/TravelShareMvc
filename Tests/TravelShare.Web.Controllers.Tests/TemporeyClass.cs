e4d860c402fa1c745bb0c2303879327e01269993
antoanLengerdjiev <alengerdjiev@gmail.com>
2017-03-17 18:01:59 +0200

    added test to controllers tests

---
+++ Tests/TravelShare.Web.Controllers.Tests/TemporeyClass.cs
@@ -0,0 +1,15 @@
+﻿namespace TravelShare.Web.Controllers.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TemporeyClass
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.IsTrue(true);
+        }
+
+    }
+}
